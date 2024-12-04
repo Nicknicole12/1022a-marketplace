6856017f2ae65e8eed29561a9a4cc0fdb3fdb54e
Nicknicole12 <116742961+Nicknicole12@users.noreply.github.com>
2024-12-04 11:29:50 -0400

    ATUALIZAÇÃO DAS CONFIGURAÇÕES

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,11 @@
-//Gabi: Eu criei uma nova rota chamada `get /usuarios` para a aplicação. Essa rota vai receber as 
-//informações do usuário, como nome e email. Também fiz uma verificação para garantir que todos os dados
-//necessários foram enviados. Se faltar alguma coisa, a rota vai retornar um erro.
+import express from 'express'
+import mysql from 'mysql2/promise'
+import cors from 'cors'
 
-//Jose: Eu fiquei responsável pela parte que insere os dados no banco de dados. 
-//Usei uma consulta SQL para salvar o nome e o email de forma segura, evitando problemas de segurança. 
+const app = express()
+app.use(express.json())
+app.use(cors())
 
-
-
-import express from 'express';
-import mysql from 'mysql2/promise';
-import cors from 'cors';
-
-const app = express();
-app.use(express.json());
-app.use(cors());
 
 app.get("/produtos", async (req, res) => {
     try {
@@ -23,15 +15,36 @@ app.get("/produtos", async (req, res) => {
             password: process.env.dbpassword ? process.env.dbpassword : "",
             database: process.env.dbname ? process.env.dbname : "banco1022a",
             port: process.env.dbport ? parseInt(process.env.dbport) : 3306
-        });
-        const [result] = await connection.query("SELECT * FROM produtos");
-        await connection.end();
-        res.send(result);
+        })
+        const [result, fields] = await connection.query("SELECT * from produtos")
+        await connection.end()
+        res.send(result)
     } catch (e) {
-        console.error(e); 
-        res.status(500).send("Server ERROR");
+        console.log(e)
+        res.status(500).send("Server ERROR")
     }
-});
+})
+app.post("/produtos", async (req, res) => {
+    try {
+        const connection = await mysql.createConnection({
+            host: process.env.dbhost ? process.env.dbhost : "localhost",
+            user: process.env.dbuser ? process.env.dbuser : "root",
+            password: process.env.dbpassword ? process.env.dbpassword : "",
+            database: process.env.dbname ? process.env.dbname : "banco1022a",
+            port: process.env.dbport ? parseInt(process.env.dbport) : 3306
+        })
+        const {id,nome,descricao,preco,imagem} = req.body
+        const [result, fields] = 
+                    await connection.query("INSERT INTO produtos VALUES (?,?,?,?,?)",
+                            [id,nome,descricao,preco,imagem])
+        await connection.end()
+        res.send(result)
+    } catch (e) {
+        console.log(e)
+        res.status(500).send(e)
+    }
+})
+
 
 app.get("/usuarios", async (req, res) => {
     try {
@@ -41,16 +54,15 @@ app.get("/usuarios", async (req, res) => {
             password: process.env.dbpassword ? process.env.dbpassword : "",
             database: process.env.dbname ? process.env.dbname : "banco1022a",
             port: process.env.dbport ? parseInt(process.env.dbport) : 3306
-        });
-        const [result] = await connection.query("SELECT * FROM usuarios");
-        await connection.end();
-        res.send(result);
+        })
+        const [result, fields] = await connection.query("SELECT * from usuarios")
+        await connection.end()
+        res.send(result)
     } catch (e) {
-        console.error(e);
-        res.status(500).send("Server ERROR");
+        res.status(500).send("Server ERROR")
     }
-});
+})
 
 app.listen(8000, () => {
-    console.log("Iniciei o servidor");
-});
+    console.log("Iniciei o servidor")
+})
