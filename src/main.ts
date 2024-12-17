dffbcaf718f86b15f900bcf285f03cd785a0880b
Nicknicole12 <116742961+Nicknicole12@users.noreply.github.com>
2024-12-17 11:42:20 -0400

    mexendo no bancoMysql.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import express from 'express'
 import mysql from 'mysql2/promise'
 import cors from 'cors'
-
+import BancoMysql from "./db/bancoMysql"
 const app = express()
 app.use(express.json())
 app.use(cors())
@@ -9,15 +9,10 @@ app.use(cors())
 
 app.get("/produtos", async (req, res) => {
     try {
-        const connection = await mysql.createConnection({
-            host: process.env.dbhost ? process.env.dbhost : "localhost",
-            user: process.env.dbuser ? process.env.dbuser : "root",
-            password: process.env.dbpassword ? process.env.dbpassword : "",
-            database: process.env.dbname ? process.env.dbname : "banco1022a",
-            port: process.env.dbport ? parseInt(process.env.dbport) : 3306
-        })
-        const [result, fields] = await connection.query("SELECT * from produtos")
-        await connection.end()
+        const banco = new BancoMysql()
+      await  banco.criarConexao()
+    const result =  await banco.consultar()
+      await banco.FinalizarConexao()
         res.send(result)
     } catch (e) {
         console.log(e)
